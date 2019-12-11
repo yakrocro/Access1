6abac7ec9607245581e0ece74b8191a36065f263
Junior <kouasjunior@gmail.com>
2019-12-11 10:56:55 -0500

    add confirm component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ConfirmCodeComponent } from './confirm-code/confirm-code.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponent,
+    ConfirmCodeComponent,
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
---
+++ src/app/confirm-code/confirm-code.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-code',
+  templateUrl: './confirm-code.component.html',
+  styleUrls: ['./confirm-code.component.scss']
+})
+export class ConfirmCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
