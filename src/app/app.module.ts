34d18fd5097e6a3a5891aa5f3248e39a6216a4b7
Junior <kouasjunior@gmail.com>
2019-12-11 00:32:39 -0500

    change participant html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ParticipantComponent } from './participant/participant/participant.component';
-// import { CodeInComponent } from './code-in/code-in.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponent,
-    ParticipantComponent,
-    // CodeInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
