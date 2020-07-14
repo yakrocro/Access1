a6fc93476da726e0270f1cff9894ce5d256e8db5
Junior <kouasjunior@gmail.com>
2020-07-13 23:46:47 -0400

    save changes on 20200713

--- src/app/class/AccessData.ts
+++ src/app/class/AccessData.ts
@@ -1,5 +1,15 @@
 export interface IaccessData {
+  // code: string,
+  // date: string,
+  // heure: string,
+  // sense: string
   code: string,
+  etat: string,
+  // mouvements: { date: string, heure: string, sense: string } [];
+  mouvements: Imouvement[];
+}
+
+export interface Imouvement {
   date: string,
   heure: string,
   sense: string
--- src/app/code-in/code-in.component.ts
+++ src/app/code-in/code-in.component.ts
@@ -11,8 +11,11 @@ import { IaccessData } from '../class/AccessData';
 export class CodeInComponent implements OnInit {
 
   private _showConfirmModal : boolean = false;
-  private sense : string = "Arrivée" ;
-  public message : string ;
+  private ETAT_ENTREE = "entree";
+  private ETAT_SORTIE = "sortie";
+  private DEFAULT_ETAT = this.ETAT_SORTIE;
+  private DEFAULT_MESSAGE = "Bon matin!"
+  public message : string = this.DEFAULT_MESSAGE;
   private code: string ;
   private arrivees: IaccessData[] = [] ;
   private data : IaccessData ;
@@ -20,11 +23,12 @@ export class CodeInComponent implements OnInit {
   constructor () { }
 
   ngOnInit() {
-    // localStorage.removeItem("arrivees");
+    localStorage.removeItem("arrivees");
     this.arrivees = localStorage.getItem("arrivees") !== null
-    ? JSON.parse(localStorage.getItem("arrivees"))
-    : [] ;
+    ? JSON.parse(localStorage.getItem("arrivees")) : [] ;
+    // console.log("les arrivées => ")
     console.log(this.arrivees);
+    // this.message = "Bon matin";
   }
 
   codePartictipantForm = new FormGroup({
@@ -34,41 +38,85 @@ export class CodeInComponent implements OnInit {
   onSubmit(){
     var form = this.codePartictipantForm.value ;
     this.code = form.code;
-    if(this.isAlreadyEntered()){
-      this.message = "Votre entrée a déja été enregistrée.";
-    } else {
-      // var date = new Date().toLocaleDateString();
-      // var heure = new Date().toLocaleTimeString();
-      // this.data = {
-      //   "code":this.code,
-      //   "date":this.dateISO(date),
-      //   "heure": heure,
-      //   "sense": this.sense
-      // };
-      // this.storeData(this.data);
-
-      this.message = "Votre entrée a été enregistrée avec succès. Merci.";
+    let etat = this.lireEtatUtilisateur();
+    // if( etat === "sortie" ){
+    //    this.message = "confirmer sortie";
+    // }
+    // if(this.isAlreadyArrived()){
+    //   this.message = "Votre entrée a déja été enregistrée.";
+    //} else {
+    console.log("etat av => " + etat);
+    etat = this.changerEtat(etat);
+    var date = new Date().toLocaleDateString();
+    var heure = new Date().toLocaleTimeString();
+    console.log("etat ap => " + etat);
+    // this.data = {
+    //   "code":this.code,
+    //   "date":this.dateISO(date),
+    //   "heure": heure,
+    //   "sense": this.sense
+    // };
+    this.data = {
+      "code":this.code,
+      "etat":etat,
+      "mouvements":[
+        {
+          "date":this.dateISO(date),
+          "heure":heure,
+          "sense":etat,
+        }
+      ]
     }
+
+    //console.log(JSON.stringify(this.data));
+    this.storeData(this.data);
+    this.message = "Votre entrée a été enregistrée avec succès. Merci.";
+    // }
     this._showConfirmModal=false;
-    this.resetForm();
+    setTimeout( () => { this.resetForm(); }, 5000 );
   }
 
-onSubmit1(){
-  var form = this.codePartictipantForm.value ;
-  this.code = form.code;
-  console.log(this.isAlreadyArrived());
-  this._showConfirmModal = false ;
-  this.message = "Votre entrée a été enregistrée avec succès. Merci.";
-  setTimeout( () => { this.resetForm(); }, 5000 );
+  onSubmit1(){
+    var form = this.codePartictipantForm.value ;
+    this.code = form.code;
+    // console.log(this.isAlreadyArrived());
+    this._showConfirmModal = false ;
+    this.message = "Votre entrée a été enregistrée avec succès. Merci.";
+    setTimeout( () => { this.resetForm(); }, 5000 );
 
-}
+  }
+
+  storeData(data){
+    // this.arrivees.push(data);
+    let code = this.code.toString();
+    let trouve = false;
+
+    let index = this.arrivees.findIndex( (user) => {
+      return user.code === this.code;
+    });
+
+    if(index == -1 ){
+      this.arrivees.push(data);
+    } else {
+      this.arrivees[index].etat = data.etat;
+      this.arrivees[index].mouvements.push(data.mouvements[0]);
+    }
+    // if(this.arrivees.code)
+    // this.arrivees = data;
+    localStorage.setItem("arrivees", JSON.stringify(this.arrivees));
+    console.log(this.arrivees);
+  }
+
   public showConfirmModal(){
     return this._showConfirmModal = !this._showConfirmModal;
   }
 
-  storeData(data){
-    this.arrivees.push(data);
-    localStorage.setItem("arrivees", JSON.stringify(this.arrivees));
+  public enregistrer(){
+    this.code = this.codePartictipantForm.value.code ;
+    //console.log(this.code);
+    if( this.isEmpty(this.code))
+    this.message = "Attention, aucun code saisi.";
+    else this.showConfirmModal();
   }
 
   dateISO(date: string){
@@ -78,19 +126,42 @@ onSubmit1(){
 
   isAlreadyArrived(){
     let found = this.arrivees.find(
-      arrivee => arrivee.code === this.code && arrivee.sense === 'Arrivée'
-    );
-    if ( found !== undefined ) return true ;
-    return false ;
+      (user) => {
+        return user.code === this.code &&
+        user.mouvements[user.mouvements.length -1].sense === this.ETAT_ENTREE;
+      });
+      if( found !== undefined ) {
+
+        return true ;
+      }
+      return false ;
+    }
+
+    lireEtatUtilisateur(){
+      let user = this.arrivees.find(
+        (u) => {
+          return u.code === this.code;
+        }
+      );
+      // console.log("user => " + JSON.stringify(user));
+      if( user !== undefined ) return user.etat;
+      return this.DEFAULT_ETAT;
+    }
+
+    changerEtat(etat : string){
+       if(etat === this.ETAT_ENTREE) return this.ETAT_SORTIE;
+       return this.ETAT_ENTREE;
+    }
+
+    resetForm(){
+      this.codePartictipantForm.reset();
+      this.message = "";
+    }
+
+    private isEmpty(val){
+      if( val === "" || val === null || val == 'undefined')
+      return true;
+      return false;
+    }
+
   }
-
-  resetForm(){
-    this.codePartictipantForm.reset();
-    this.message = "";
-  }
-
-//   $('#myModal').on('shown.bs.modal', function () {
-//   $('#myInput').trigger('focus')
-// })
-
-}
